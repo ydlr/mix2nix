0ade75bc52c486ced15ae68088edb5d4b6e174e3
ydlr <ydlr@ydlr.io>
2021-05-08 21:01:40 -0500

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mix2nix.MixProject do
 	def project do
 		[
 			app: :mix2nix,
-			version: "0.1.2",
+			version: "0.1.3",
 			elixir: "~> 1.9",
 			start_permanent: Mix.env() == :prod,
 			escript: [main_module: Mix2nix.CLI]
