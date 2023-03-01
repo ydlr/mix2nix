784e795b6525dbf1ddcabc85363d70d70371f25e
tkachuk-labs <tkachuk.labs@gmail.com>
2023-03-01 10:42:51 -0300

    using hex_core package to better interact with hex

--- lib/mix2nix.ex
+++ lib/mix2nix.ex
@@ -1,145 +1,156 @@
 defmodule Mix2nix do
-	def process(filename) do
-		filename
-		|> read
-		|> expression_set
-	end
+  def process(filename) do
+    filename
+    |> read
+    |> expression_set
+  end
 
-	def expression_set(deps) do
-		deps
-		|> Map.to_list()
-		|> Enum.sort(:asc)
-		|> Enum.map(fn {_, v} -> nix_expression(deps, v) end)
-		|> Enum.reject(fn x -> x == "" end)
-		|> Enum.join("\n")
-		|> String.trim("\n")
-		|> wrap
-	end
+  def expression_set(deps) do
+    deps
+    |> Map.to_list()
+    |> Enum.sort(:asc)
+    |> Enum.map(fn {_, v} -> nix_expression(deps, v) end)
+    |> Enum.reject(fn x -> x == "" end)
+    |> Enum.join("\n")
+    |> String.trim("\n")
+    |> wrap
+  end
 
-	defp read(filename) do
-		opts = [file: filename, warn_on_unnecessary_quotes: false]
+  defp read(filename) do
+    opts = [file: filename, warn_on_unnecessary_quotes: false]
 
-		with {:ok, contents} <- File.read(filename),
-		     {:ok, quoted} <- Code.string_to_quoted(contents, opts),
-		     {%{} = lock, _} <- Code.eval_quoted(quoted, opts) do
-			lock
-		else
-			{:error, posix} when is_atom(posix) ->
-				:file.format_error(posix) |> to_string() |> IO.puts()
-				System.halt(1)
+    with {:ok, contents} <- File.read(filename),
+         {:ok, quoted} <- Code.string_to_quoted(contents, opts),
+         {%{} = lock, _} <- Code.eval_quoted(quoted, opts) do
+      lock
+    else
+      {:error, posix} when is_atom(posix) ->
+        :file.format_error(posix) |> to_string() |> IO.puts()
+        System.halt(1)
 
-			{:error, {line, error, token}} when is_integer(line) ->
-				IO.puts("Error on line #{line}: #{error} (" <> inspect(token) <> ")")
-				System.halt(1)
-		end
-	end
+      {:error, {line, error, token}} when is_integer(line) ->
+        IO.puts("Error on line #{line}: #{error} (" <> inspect(token) <> ")")
+        System.halt(1)
+    end
+  end
 
-	def is_required(allpkgs, [ hex: name, repo: _, optional: optional ]) do
-		Map.has_key?(allpkgs, name) or ! optional
-	end
+  def is_required(allpkgs, hex: name, repo: _, optional: optional) do
+    Map.has_key?(allpkgs, name) or !optional
+  end
 
-	def dep_string(allpkgs, deps) do
-		depString =
-			deps
-			|> Enum.filter(fn x -> is_required(allpkgs, elem(x, 2)) end)
-			|> Enum.map(fn x -> Atom.to_string(elem(x, 0)) end)
-			|> Enum.join(" ")
+  def dep_string(allpkgs, deps) do
+    depString =
+      deps
+      |> Enum.filter(fn x -> is_required(allpkgs, elem(x, 2)) end)
+      |> Enum.map(fn x -> Atom.to_string(elem(x, 0)) end)
+      |> Enum.join(" ")
 
-		if String.length(depString) > 0 do
-			"[ " <> depString <> " ]"
-		else
-			"[]"
-		end
-	end
+    if String.length(depString) > 0 do
+      "[ " <> depString <> " ]"
+    else
+      "[]"
+    end
+  end
 
-	def specific_workaround(pkg) do
-		case pkg do
-			"cowboy" -> "buildErlangMk"
-			"ssl_verify_fun" -> "buildRebar3"
-			"jose" -> "buildMix"
-			_ -> false
-		end
-	end
+  def specific_workaround(pkg) do
+    case pkg do
+      "cowboy" -> "buildErlangMk"
+      "ssl_verify_fun" -> "buildRebar3"
+      "jose" -> "buildMix"
+      _ -> false
+    end
+  end
 
-	def get_build_env(builders, pkgname) do
-		cond do
-			specific_workaround(pkgname) ->
-				specific_workaround(pkgname)
-			Enum.member?(builders, :mix) ->
-				"buildMix"
-			Enum.member?(builders, :rebar3) or Enum.member?(builders, :rebar) ->
-				"buildRebar3"
-			Enum.member?(builders, :make) ->
-				"buildErlangMk"
-			true ->
-				"buildMix"
-		end
-	end
+  def get_build_env(builders, pkgname) do
+    cond do
+      specific_workaround(pkgname) ->
+        specific_workaround(pkgname)
 
-	def get_hash(name, version) do
-		url = "https://repo.hex.pm/tarballs/#{name}-#{version}.tar"
-		{ result, status } = System.cmd("nix-prefetch-url", [url])
+      Enum.member?(builders, :mix) ->
+        "buildMix"
 
-		case status do
-			0 ->
-				String.trim(result)
-			_ ->
-				IO.puts("Use of nix-prefetch-url failed.")
-				System.halt(1)
-		end
-	end
+      Enum.member?(builders, :rebar3) or Enum.member?(builders, :rebar) ->
+        "buildRebar3"
 
-	def nix_expression(
-		allpkgs,
-		{:hex, name, version, _hash, builders, deps, "hexpm", hash2}
-	), do: get_hexpm_expression(allpkgs, name, version, builders, deps, hash2)
+      Enum.member?(builders, :make) ->
+        "buildErlangMk"
 
-	def nix_expression(
-		allpkgs,
-		{:hex, name, version, _hash, builders, deps, "hexpm"}
-	), do: get_hexpm_expression(allpkgs, name, version, builders, deps)
+      true ->
+        "buildMix"
+    end
+  end
 
-	def nix_expression(_allpkgs, _pkg) do
-		""
-	end
+  def get_hash(name, version) do
+    {200, %{}, tar} = :hex_client.get_repo_tarball(name, version)
+    Temp.track!()
+    {:ok, _, tmp} = Temp.open()
+    :ok = File.write!(tmp, tar)
+    {result, status} = System.cmd("nix-prefetch-url", ["file://" <> tmp])
+    Temp.cleanup()
 
-	defp get_hexpm_expression(allpkgs, name, version, builders, deps, sha256 \\ nil) do
-		name = Atom.to_string(name)
-		buildEnv = get_build_env(builders, name)
-		sha256 = sha256 || get_hash(name, version)
-		deps = dep_string(allpkgs, deps)
+    case status do
+      0 ->
+        String.trim(result)
 
-		"""
-		    #{name} = #{buildEnv} rec {
-		      name = "#{name}";
-		      version = "#{version}";
+      _ ->
+        IO.puts("Use of nix-prefetch-url failed.")
+        System.halt(1)
+    end
+  end
 
-		      src = fetchHex {
-		        pkg = "${name}";
-		        version = "${version}";
-		        sha256 = "#{sha256}";
-		      };
+  def nix_expression(
+        allpkgs,
+        {:hex, name, version, _hash, builders, deps, "hexpm", hash2}
+      ),
+      do: get_hexpm_expression(allpkgs, name, version, builders, deps, hash2)
 
-		      beamDeps = #{deps};
-		    };
-		"""
-	end
+  def nix_expression(
+        allpkgs,
+        {:hex, name, version, _hash, builders, deps, "hexpm"}
+      ),
+      do: get_hexpm_expression(allpkgs, name, version, builders, deps)
 
-	defp wrap(pkgs) do
-		"""
-		{ lib, beamPackages, overrides ? (x: y: {}) }:
+  def nix_expression(_allpkgs, _pkg) do
+    ""
+  end
 
-		let
-		  buildRebar3 = lib.makeOverridable beamPackages.buildRebar3;
-		  buildMix = lib.makeOverridable beamPackages.buildMix;
-		  buildErlangMk = lib.makeOverridable beamPackages.buildErlangMk;
+  defp get_hexpm_expression(allpkgs, name, version, builders, deps, sha256 \\ nil) do
+    name = Atom.to_string(name)
+    buildEnv = get_build_env(builders, name)
+    sha256 = sha256 || get_hash(name, version)
+    deps = dep_string(allpkgs, deps)
 
-		  self = packages // (overrides self packages);
+    """
+        #{name} = #{buildEnv} rec {
+          name = "#{name}";
+          version = "#{version}";
 
-		  packages = with beamPackages; with self; {
-		#{pkgs}
-		  };
-		in self
-		"""
-	end
+          src = fetchHex {
+            pkg = "${name}";
+            version = "${version}";
+            sha256 = "#{sha256}";
+          };
+
+          beamDeps = #{deps};
+        };
+    """
+  end
+
+  defp wrap(pkgs) do
+    """
+    { lib, beamPackages, overrides ? (x: y: {}) }:
+
+    let
+      buildRebar3 = lib.makeOverridable beamPackages.buildRebar3;
+      buildMix = lib.makeOverridable beamPackages.buildMix;
+      buildErlangMk = lib.makeOverridable beamPackages.buildErlangMk;
+
+      self = packages // (overrides self packages);
+
+      packages = with beamPackages; with self; {
+    #{pkgs}
+      };
+    in self
+    """
+  end
 end
--- mix.exs
+++ mix.exs
@@ -1,20 +1,28 @@
 defmodule Mix2nix.MixProject do
-	use Mix.Project
+  use Mix.Project
 
-	def project do
-		[
-			app: :mix2nix,
-			version: "0.1.6",
-			elixir: "~> 1.9",
-			start_permanent: Mix.env() == :prod,
-			escript: [main_module: Mix2nix.CLI]
-		]
-	end
+  def project do
+    [
+      app: :mix2nix,
+      version: "0.1.6",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      escript: [main_module: Mix2nix.CLI],
+      deps: deps()
+    ]
+  end
 
-	# Run "mix help compile.app" to learn about applications.
-	def application do
-		[
-			extra_applications: [:logger]
-		]
-	end
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:hex_core, "~> 0.9"},
+      {:temp, "~> 0.4"}
+    ]
+  end
 end
