8b9485d38d9eeb0c3f44f9b838f8dbd37105dce7
tkachuk-labs <tkachuk.labs@gmail.com>
2023-03-03 15:01:12 -0300

    fix test

--- test/result.nix
+++ test/result.nix
@@ -1,9 +1,81 @@
-{ lib, beamPackages, overrides ? (x: y: {}) }:
+{ lib,
+  beamPackages,
+  overrides ? (x: y: {}),
+  #
+  # NOTE : for private hex org pkgs only
+  #
+  stdenv ? null,
+  stdenvNoCC ? null,
+  mix2nix ? null,
+  hexOrg ? null,
+  hexKey ? null
+}:
 
 let
   buildRebar3 = lib.makeOverridable beamPackages.buildRebar3;
   buildMix = lib.makeOverridable beamPackages.buildMix;
   buildErlangMk = lib.makeOverridable beamPackages.buildErlangMk;
+
+  fetchHexOrg = ({
+    pkg
+    , version
+    , sha256
+    , meta ? { }
+    , hexOrg
+    , hexKey
+    }:
+
+    let fetchHexCore =
+      stdenvNoCC.mkDerivation (rec {
+        name = "${pkg}-${version}.tar";
+        buildCommand = ''
+          ${mix2nix}/bin/mix2nix \
+            --hex-pkg-get ${pkg} \
+            --hex-pkg-vsn ${version} \
+            --hex-pkg-org ${hexOrg} \
+            --hex-api-key ${hexKey}
+          mv ${name} $out
+        '';
+        outputHashAlgo = "sha256";
+        outputHash = sha256;
+      });
+
+    in
+      stdenv.mkDerivation ({
+        pname = "hex-source-${pkg}";
+        inherit version;
+        dontBuild = true;
+        dontConfigure = true;
+        dontFixup = true;
+
+        src = fetchHexCore;
+
+        unpackCmd = ''
+          tar -xf $curSrc contents.tar.gz CHECKSUM metadata.config
+          mkdir contents
+          tar -C contents -xzf contents.tar.gz
+          mv metadata.config contents/hex_metadata.config
+
+          # To make the extracted hex tarballs appear legitimate to mix, we need to
+          # make sure they contain not just the contents of contents.tar.gz but also
+          # a .hex file with some lock metadata.
+          # We use an old version of .hex file per hex's mix_task_test.exs since it
+          # is just plain-text instead of an encoded format.
+          # See: https://github.com/hexpm/hex/blob/main/test/hex/mix_task_test.exs#L410
+          echo -n "${pkg},${version},$(cat CHECKSUM | tr '[:upper:]' '[:lower:]'),hexpm" > contents/.hex
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir "$out"
+          cp -Hrt "$out" .
+          success=1
+          runHook postInstall
+        '';
+
+        inherit meta;
+      })
+  );
 
   self = packages // (overrides self packages);
 
