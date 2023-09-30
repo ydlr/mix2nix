18d3b4a9e035786f732133c608035353b0fa2898
Zach <zach@hipcreativeinc.com>
2023-09-30 11:16:21 -0500

    bump version to 0.1.8

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 build:
 	nix-build
 
+test:
+	nix-shell --run "mix test"
+
 install:
 	nix-env -f ./ -i
+
+.PHONY: build test install
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mix2nix.MixProject do
 	def project do
 		[
 			app: :mix2nix,
-			version: "0.1.7",
+			version: "0.1.8",
 			elixir: "~> 1.9",
 			start_permanent: Mix.env() == :prod,
 			escript: [main_module: Mix2nix.CLI]
