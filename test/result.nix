6386232176ca0c9aecf9f403ad4ff2e7f8ec840e
Andrew Bruce <me@andrewbruce.net>
2023-11-12 10:12:41 +0000

    Add grpcbox and its dependencies to test fixture

--- test/result.nix
+++ test/result.nix
@@ -8,6 +8,32 @@ let
   self = packages // (overrides self packages);
 
   packages = with beamPackages; with self; {
+    acceptor_pool = buildRebar3 rec {
+      name = "acceptor_pool";
+      version = "1.0.0";
+
+      src = fetchHex {
+        pkg = "acceptor_pool";
+        version = "${version}";
+        sha256 = "0cbcd83fdc8b9ad2eee2067ef8b91a14858a5883cb7cd800e6fcd5803e158788";
+      };
+
+      beamDeps = [];
+    };
+
+    chatterbox = buildRebar3 rec {
+      name = "chatterbox";
+      version = "0.13.0";
+
+      src = fetchHex {
+        pkg = "ts_chatterbox";
+        version = "${version}";
+        sha256 = "b93d19104d86af0b3f2566c4cba2a57d2e06d103728246ba1ac6c3c0ff010aa7";
+      };
+
+      beamDeps = [ hpack ];
+    };
+
     cowboy = buildErlangMk rec {
       name = "cowboy";
       version = "2.9.0";
@@ -29,6 +55,19 @@ let
         pkg = "cowlib";
         version = "${version}";
         sha256 = "2b3e9da0b21c4565751a6d4901c20d1b4cc25cbb7fd50d91d2ab6dd287bc86a9";
+      };
+
+      beamDeps = [];
+    };
+
+    ctx = buildRebar3 rec {
+      name = "ctx";
+      version = "0.6.0";
+
+      src = fetchHex {
+        pkg = "ctx";
+        version = "${version}";
+        sha256 = "a14ed2d1b67723dbebbe423b28d7615eb0bdcba6ff28f2d1f1b0a7e1d4aa5fc2";
       };
 
       beamDeps = [];
@@ -60,6 +99,45 @@ let
       beamDeps = [ decimal jason telemetry ];
     };
 
+    gproc = buildRebar3 rec {
+      name = "gproc";
+      version = "0.8.0";
+
+      src = fetchHex {
+        pkg = "gproc";
+        version = "${version}";
+        sha256 = "580adafa56463b75263ef5a5df4c86af321f68694e7786cb057fd805d1e2a7de";
+      };
+
+      beamDeps = [];
+    };
+
+    grpcbox = buildRebar3 rec {
+      name = "grpcbox";
+      version = "0.16.0";
+
+      src = fetchHex {
+        pkg = "grpcbox";
+        version = "${version}";
+        sha256 = "294df743ae20a7e030889f00644001370a4f7ce0121f3bbdaf13cf3169c62913";
+      };
+
+      beamDeps = [ acceptor_pool chatterbox ctx gproc ];
+    };
+
+    hpack = buildRebar3 rec {
+      name = "hpack";
+      version = "0.2.3";
+
+      src = fetchHex {
+        pkg = "hpack_erl";
+        version = "${version}";
+        sha256 = "06f580167c4b8b8a6429040df36cc93bba6d571faeaec1b28816523379cbb23a";
+      };
+
+      beamDeps = [];
+    };
+
     jason = buildMix rec {
       name = "jason";
       version = "1.4.0";
@@ -80,7 +158,7 @@ let
       src = fetchHex {
         pkg = "ranch";
         version = "${version}";
-        sha256 = "1rfz5ld54pkd2w25jadyznia2vb7aw9bclck21fizargd39wzys9";
+        sha256 = "49fbcfd3682fab1f5d109351b61257676da1a2fdbe295904176d5e521a2ddfe5";
       };
 
       beamDeps = [];
