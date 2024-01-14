ae202de4ecf82e6d4f0821d186c28f04072b4330
ydlr <ydlr@ydlr.io>
2024-01-14 14:55:10 -0600

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "mix2nix: Generate a set of nix derivations from a mix.lock file";
+
+  inputs = {
+		nixpkgs.url = "nixpkgs/nixos-23.11";
+	};
+
+  outputs = { self, nixpkgs }:
+		let
+			supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+			forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+			nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+		in {
+			packages = forAllSystems (system:
+				let
+					pkgs = nixpkgsFor.${system};
+				in {
+					default = pkgs.callPackage ./default.nix { inherit pkgs; };
+
+					devShell = pkgs.mkShell {
+						buildInputs = with pkgs; [ elixir bashInteractive ];
+					};
+				});
+		};
+}
